8857c66c5a7c4f83801b1ae7dd0844631867d234
Max Lund <lundmax@gmail.com>
2026-03-20 21:55:02 -0500

    Add parent process monitoring to prevent orphaned plugins

--- serve.go
+++ serve.go
@@ -1,12 +1,20 @@
 package squadron
 
 import (
+	"os"
+	"time"
+
 	"github.com/hashicorp/go-plugin"
 )
 
 // Serve starts the plugin server with the given ToolProvider implementation.
 // This is the main entry point for plugin binaries.
+// It also monitors the parent process and exits if the parent dies,
+// preventing orphaned plugin processes.
 func Serve(impl ToolProvider) {
+	// Monitor parent process - exit if parent dies
+	go monitorParent()
+
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: Handshake,
 		Plugins: map[string]plugin.Plugin{
@@ -14,4 +22,20 @@ func Serve(impl ToolProvider) {
 		},
 		GRPCServer: plugin.DefaultGRPCServer,
 	})
+}
+
+// monitorParent checks if the parent process is still alive.
+// If the parent dies (PPID becomes 1 on Unix), the plugin exits.
+func monitorParent() {
+	ppid := os.Getppid()
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		currentPPID := os.Getppid()
+		// On Unix, when the parent dies, PPID becomes 1 (init/launchd)
+		if currentPPID != ppid || currentPPID == 1 {
+			os.Exit(0)
+		}
+	}
 }
